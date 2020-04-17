73121a73595b6fdcc1bbe0117c13663e49aa617e
Jhoterdan de Paula <jotaskt@gmail.com>
2020-04-17 00:50:22 -0300

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$errors = array();  	// array to hold validation errors
-//$data = array(); 		// array to pass back data
-echo "YEYE";
-$temp_file = fopen("rsa_temp.txt","a");
-fwrite($temp_file,"jskjkdkasndkasndjkasndjkasn");
-fclose($temp_file);
